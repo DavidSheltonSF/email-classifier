6fcff6e3f7ae2db6f6f23c9c56936cf5aefac7b2
DavidSheltonSF <david.faria012@gmail.com>
2025-08-28 19:16:04 -0300

    feat(controllers): add classify_controller

---
+++ src/controllers/classify_controller.py
@@ -0,0 +1,21 @@
+from fastapi import APIRouter
+from fastapi.responses import JSONResponse
+from src.services.EmailClassifier.classifier import EmailClassifierService
+from src.services.errors.email import ApplicationError
+
+router = APIRouter(prefix='/classify', tags=['classify'])
+classifier = EmailClassifierService()
+
+@router.post('/email')
+def classify_email(email: dict):
+  try:
+    result = classifier.execute(email)
+    return JSONResponse(
+      status_code=200,
+      content={'status': 'success', 'result': result}
+    )
+  except ApplicationError as e:
+    return JSONResponse(
+      status_code=e.status_code,
+      content={'status': 'error', 'messge': e.message}
+    )
