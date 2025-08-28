07dc619fff474547dcdb9fa9fed8f8ad94b61670
DavidSheltonSF <david.faria012@gmail.com>
2025-08-28 19:16:18 -0300

    refactor(main): remove classify endpoint and integrate classify_controller

--- src/main.py
+++ src/main.py
@@ -1,21 +1,6 @@
 from fastapi import FastAPI
-from fastapi.responses import JSONResponse
-from src.services.EmailClassifier.classifier import EmailClassifierService
-from src.services.errors.email import ApplicationError
+from src.controllers import classify_controller
 
 app = FastAPI()
-classifier = EmailClassifierService()
 
-@app.post('/classify')
-def classify(email: dict):
-  try:
-    result = classifier.execute(email)
-    return JSONResponse(
-      status_code=200,
-      content={'status': 'success', 'result': result}
-    )
-  except ApplicationError as e:
-    return JSONResponse(
-      status_code=e.status_code,
-      content={'status': 'error', 'messge': e.message}
-    )
+app.include_router(classify_controller.router)
