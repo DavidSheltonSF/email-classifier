c39b92ebd7fc01a998ffc6ea5c28b158bfbecb82
DavidSheltonSF <david.faria012@gmail.com>
2025-08-30 14:54:33 -0300

    refactor(replies): enhance replies for clarity

--- backend/src/services/helpers/repliesMap.py
+++ backend/src/services/helpers/repliesMap.py
@@ -1,10 +1,10 @@
 repliesMap = {
   "produtivo": {
     "subject": "Recebemos sua mensagem",
-    "body": "Obrigado pelo seu contato. Vamos analisar sua solicitação e retornaremos em breve."
+    "body": "Obrigado pelo seu contato. Vamos analisar sua solicitação e retornaremos em breve.\n\nAtenciosamente,\nTime AutoU"
   },
   "improdutivo": {
     "subject": "Recebemos sua mensagem",
-    "body": "Agradecemos sua mensagem, mas este canal é destinado a assuntos relacionados ao suporte/empresa."
+    "body": "Agradecemos sua mensagem, mas este canal é destinado a assuntos relacionados à empresa.\n\nAtenciosamente,\nTime AutoU"
   }
 }
