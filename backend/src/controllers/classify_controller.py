c09911ac2ceadb44af183d56f66c73ab4348112e
DavidSheltonSF <david.faria012@gmail.com>
2025-08-31 14:08:48 -0300

    refactor(classify_controller): add error hendling to classify_email_pdf

--- backend/src/controllers/classify_controller.py
+++ backend/src/controllers/classify_controller.py
@@ -24,28 +24,30 @@ def classify_email(email: dict):
   
 @router.post('/email-pdf')
 async def classify_email_pdf(file: UploadFile = File(...)):
+  try:
+    pdf_bytes = await file.read()
+    pdf_file = io.BytesIO(pdf_bytes)
+    pdf_reader = PyPDF2.PdfReader(pdf_file)
 
-  print(type(file))
-  pdf_bytes = await file.read()
-  pdf_file = io.BytesIO(pdf_bytes)
-  pdf_reader = PyPDF2.PdfReader(pdf_file)
+    email_string = ''
+    for page in pdf_reader.pages:
+      email_string += f"\n{page.extract_text()}"
 
-  email_string = ''
+    email_split = email_string.split('\n')
 
-  for page in pdf_reader.pages:
-    email_string += f"\n{page.extract_text()}"
-
-
-  email_split = email_string.split('\n')
-
-  email = {
+    email = {
     "subject": email_split.pop(0),
     "body": '\n'.join(email_split)
-  }
+    }
 
-  data = classifier.execute(email)
+    data = classifier.execute(email)
 
-  return JSONResponse(
-      status_code=200,
-      content={'status': 'success', 'data': data}
+    return JSONResponse(
+    status_code=200,
+    content={'status': 'success', 'data': data}
+    )
+  except ApplicationError as e:
+    return JSONResponse(
+      status_code=e.status_code,
+      content={'status': 'error', 'message': e.message}
     )
