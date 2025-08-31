9137410152bef6c9614488b8e9baeeb3dfbb0b0c
DavidSheltonSF <david.faria012@gmail.com>
2025-08-31 12:06:49 -0300

    feat(script): add addClassToElemets, removeClassToElements, clearFields, disableFields and enableFields helpers

---
+++ frontend/script/helpers/elementClass.js
@@ -0,0 +1,11 @@
+export function addClassToElements(elements, elementCLass) {
+  for (let element of elements){
+    element.classList.add(elementCLass);
+  }
+}
+
+export function removeClassFromElements(elements, elementCLass) {
+  for (let element of elements) {
+    element.classList.remove(elementCLass);
+  }
+}
---
+++ frontend/script/helpers/form.js
@@ -0,0 +1,18 @@
+export function clearFields(fields) {
+  for (let field of fields) { 
+    field.value = '';
+  }
+}
+
+export function disableFields(fields) {
+  for (let field of fields){
+    field.disabled = true;
+  }
+}
+
+export function enableFields(fields) {
+  for (let field of fields) {
+    field.disabled = false;
+  }
+}
+
