ebd97c7d67c06665a90e11bcfb14a6feb81d6451
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 20:41:25 -0300

    chore(main) remove hugghing face auth

--- backend/src/main.py
+++ backend/src/main.py
@@ -2,10 +2,7 @@ import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.controllers import classify_controller
-from huggingface_hub import login
 from transformers import pipeline
-
-login(os.getenv('HUGGINGFACE_HUB_TOKEN'))
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 MODEL_PATH = os.path.join(BASE_DIR, "model")
