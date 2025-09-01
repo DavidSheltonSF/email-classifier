65a1db51c756630d79d7d4482c8708231665c8e9
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 18:14:18 -0300

    fix(main): ensure model repository exists before try list it

--- backend/src/main.py
+++ backend/src/main.py
@@ -10,8 +10,9 @@ login(os.getenv('HUGGINGFACE_HUB_TOKEN'))
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 MODEL_PATH = os.path.join(BASE_DIR, "model")
 
+os.makedirs(MODEL_PATH, exist_ok=True)  # garante que a pasta exista
+
 if not os.listdir(MODEL_PATH):
-  os.makedirs(MODEL_PATH, exist_ok=True)  # garante que a pasta exista
   print("Model path:", MODEL_PATH)
 
   # Se for privado, precisa do token: export HF_TOKEN=xxx
