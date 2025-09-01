15a189920b0fd2ea4838a706f58355a70ffc8468
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 18:01:44 -0300

    refactor: create model directly in main file and delete download_model_from_hub.py

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,7 +1,27 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.controllers import classify_controller
-import src.download_model_from_hub
+from huggingface_hub import login
+from transformers import pipeline
+
+login(os.getenv('HUGGINGFACE_HUB_TOKEN'))
+
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+MODEL_PATH = os.path.join(BASE_DIR, "model")
+
+if not os.listdir(MODEL_PATH):
+  os.makedirs(MODEL_PATH, exist_ok=True)  # garante que a pasta exista
+  print("Model path:", MODEL_PATH)
+
+  # Se for privado, precisa do token: export HF_TOKEN=xxx
+  classifier = pipeline(
+      'text-classification', 
+      model="davidshelton/email-classifier-soft-test", 
+      tokenizer="davidshelton/email-classifier-soft-test"
+  )
+
+  classifier.save_pretrained(MODEL_PATH)
 
 app = FastAPI()
 
