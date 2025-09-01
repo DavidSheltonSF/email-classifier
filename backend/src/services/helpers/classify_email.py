c2eeb026a63458696a2a42161d43504f406c97dc
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 01:03:12 -0300

    refactor(classify_email) modify model path file

--- backend/src/services/helpers/classify_email.py
+++ backend/src/services/helpers/classify_email.py
@@ -5,8 +5,8 @@ from src.services.serviceTypes.EmailClassification import EmailClassification
 def classify_email(subject, body) -> EmailClassification:
   email = f"{subject} {body}"
   BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
-  MODEL_PATH = os.path.join(BASE_DIR, "data", "model")
-  TOKENIZER_PATH = os.path.join(BASE_DIR, "data", "tokenizer")
+  MODEL_PATH = os.path.join(BASE_DIR, "model")
+  TOKENIZER_PATH = os.path.join(BASE_DIR, "model")
 
   model = AutoModelForSequenceClassification.from_pretrained(MODEL_PATH, id2label={0: 'produtivo', 1: 'improdutivo'})
   tokenizer = AutoTokenizer.from_pretrained(TOKENIZER_PATH)
