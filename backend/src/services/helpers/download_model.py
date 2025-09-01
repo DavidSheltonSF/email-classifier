faad15b5aeb0dbb4373d3b9c6b2f405236279bdf
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 20:59:03 -0300

    refactor(services): move model dowloading from main to a service helper

---
+++ backend/src/services/helpers/download_model.py
@@ -0,0 +1,21 @@
+import os
+from transformers import pipeline
+
+def download_model() :
+  BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
+  MODEL_PATH = os.path.join(BASE_DIR, "model")
+
+  os.makedirs(MODEL_PATH, exist_ok=True)  # garante que a pasta exista
+
+  if not os.listdir(MODEL_PATH):
+    print("Model path:", MODEL_PATH)
+
+    # Se for privado, precisa do token: export HF_TOKEN=xxx
+    classifier = pipeline(
+        'text-classification', 
+        model="davidshelton/email-classifier-soft-test", 
+        tokenizer="davidshelton/email-classifier-soft-test"
+    )
+
+    classifier.save_pretrained(MODEL_PATH)
+
