275f6b784ffb6c81006ef31ae9d126a68bc83ac6
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 16:31:06 -0300

    feat(routes): add route to train model

--- backend/src/controllers/classify_controller.py
+++ backend/src/controllers/classify_controller.py
@@ -9,6 +9,7 @@ from fastapi.responses import JSONResponse
 from src.services.EmailClassifier.classifier import EmailClassifierService
 from src.services.errors.email import ApplicationError
 from src.services.helpers.download_model_from_hub import download_model_from_hub
+from src.services.helpers.train_model_and_upload import train_model_and_upload
 import pandas as pd
 from huggingface_hub import login
 
@@ -110,6 +111,27 @@ def download_model():
     return JSONResponse(
       status_code=200,
       content={'status': 'error', 'message': 'model was not downloaded'}
+    )
+
+  except ApplicationError as e:
+    return JSONResponse(
+      status_code=e.status_code,
+      content={'status': 'error', 'message': e.message}
+    )
+  
+@router.post('/train-model')
+def train_model():
+  try:
+    result = train_model_and_upload()
+    if(result):
+      return JSONResponse(
+      status_code=200,
+      content={'status': 'success', 'message': 'model trained successfuly'}
+    )
+
+    return JSONResponse(
+      status_code=200,
+      content={'status': 'error', 'message': 'model was not trained'}
     )
 
   except ApplicationError as e:
---
+++ backend/src/services/helpers/train_model_and_upload.py
@@ -0,0 +1,81 @@
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, Trainer, TrainingArguments
+from datasets import load_dataset, Dataset
+from huggingface_hub import login
+from dotenv import load_dotenv
+import os
+
+def train_model_and_upload():
+  try:
+    load_dotenv()
+    login(os.getenv('HUGGINGFACE_HUB_TOKEN'))
+
+    # BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+    # train_path = os.path.join(BASE_DIR, "train.csv")
+    # test_path = os.path.join(BASE_DIR, "test.csv")
+
+    # dataset = load_dataset("csv", data_files={"train": train_path, "test": test_path})
+    dataset = load_dataset('davidshelton/email-classifier-dataset')
+
+    model_name = "adalbertojunior/distilbert-portuguese-cased"
+    # model_name = "neuralmind/bert-base-portuguese-cased"
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=2)
+
+    def mapCol(batch):
+      batch['label'] = [0 if l == 'produtivo' else 1 for l in batch['label']]
+      return batch
+
+    def combineSubjectAndBody(batch):
+      batch['text'] = [f"{sub.lower()} {body.lower()}" for sub, body in zip(batch['subject'], batch['body'])]
+      return batch
+
+    def tokenize(batch):
+      return tokenizer(batch['text'], padding=True, truncation=True)
+
+    dataset = dataset.map(mapCol, batched=True)
+    dataset = dataset.map(combineSubjectAndBody, batched=True)
+    dataset = dataset.map(tokenize, batched=True)
+
+    training_args = TrainingArguments(
+      output_dir='./data/results',
+      eval_strategy="epoch",
+      learning_rate=5e-5,
+      per_device_train_batch_size=4, #4 ou 8
+      num_train_epochs=2,
+      weight_decay=0.03,
+      push_to_hub=True,
+      hub_model_id="davidshelton/email-classifier-soft"
+    )
+
+    test_training_args = TrainingArguments(
+        output_dir='./data/results',
+        eval_strategy="steps",   # troca "epoch" por "steps" para avaliar mais cedo
+        eval_steps=20,                 # avalia a cada 20 steps
+        logging_steps=10,              # loga a cada 10 steps
+        save_strategy="no",            # não salva checkpoints intermediários
+        learning_rate=5e-5,
+        per_device_train_batch_size=2, # menor batch → menos memória
+        num_train_epochs=1,            # só 1 época para testar
+        max_steps=50,                  # para cedo (50 batches no máx.)
+        weight_decay=0.01,
+        push_to_hub=True,
+        hub_model_id="davidshelton/email-classifier-soft-test"  # repositório no Hugging Face
+    )
+
+    small_train = dataset['train'].shuffle(seed=42).select(range(100))
+    small_test = dataset['test'].shuffle(seed=42).select(range(50))
+
+    trainer = Trainer(
+      model=model,
+      args=test_training_args,
+      train_dataset=small_train,
+      eval_dataset=small_test,
+    )
+
+    trainer.train()
+    trainer.push_to_hub()
+
+    return True
+  except:
+    return False
+
