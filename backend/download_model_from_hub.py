44100391e996a91272bf97baf38fa7af990e39d4
DavidSheltonSF <david.faria012@gmail.com>
2025-09-01 17:25:40 -0300

    fix: add auth to download_model_from_hub.py

--- backend/download_model_from_hub.py
+++ backend/download_model_from_hub.py
@@ -1,5 +1,11 @@
 import os
 from transformers import pipeline
+from dotenv import load_dotenv
+from huggingface_hub import login
+
+load_dotenv()
+
+login(os.getenv('HUGGINGFACE_HUB_TOKEN'))
 
 BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 MODEL_PATH = os.path.join(BASE_DIR, "model")
