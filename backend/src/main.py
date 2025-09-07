d29b7f832a8e1fd82ca99e7cbba59be6963fcfbf
DavidSheltonSF <david.faria012@gmail.com>
2025-09-07 18:19:05 -0300

    chore(main): remove unused imports and unnecessary comment

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,8 @@
-import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from src.controllers import classify_controller
-from src.services.helpers.download_model import download_model
 from backend.src.services.helpers.ClassifierModel import ClassifierModel
 
-#download_model()
 
 classifier = ClassifierModel()
 classifier.load_model()
