d1b01f34aa056166152b0d8b349d0229fed660b1
DavidSheltonSF <david.faria012@gmail.com>
2025-09-07 18:14:58 -0300

    feat(ClassifierModel): add load_model_from_hub_and_save method

--- backend/src/services/helpers/ClassifierModel.py
+++ backend/src/services/helpers/ClassifierModel.py
@@ -20,6 +20,22 @@ class ClassifierModel:
       cls._instance.model = None
     return cls._instance
   
+  def load_model_from_hub_and_save(self):
+    if(self.model):
+      return 
+
+    tokenizer = AutoTokenizer.from_pretrained(self._repository_link)
+    model = AutoModelForSequenceClassification.from_pretrained(
+      self._repository_link,
+    )
+  
+    self.model = pipeline(
+        'text-classification', 
+        model=model, 
+        tokenizer=tokenizer
+    )
+    self.model.save_pretrained(self._local_model_path)
+  
   def load_model_from_hub(self):
     if(self.model):
       return 
