1d1b34cbe15d1f91be8544224c296f4e86632bb0
DavidSheltonSF <david.faria012@gmail.com>
2025-09-30 20:42:28 -0300

    refactor(ClassifierModel): add attribute to define labels identifiers

--- backend/src/services/helpers/ClassifierModel.py
+++ backend/src/services/helpers/ClassifierModel.py
@@ -5,6 +5,7 @@ class ClassifierModel:
   _instance = None
   _repository_link = 'davidshelton/email-classifier-soft'
   _local_model_path = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../', 'model'))
+  _label_identifiers = {0: "ham", 1: "spam"}
 
   def __new__(cls):
     if cls._instance is None:
@@ -35,7 +36,7 @@ class ClassifierModel:
     tokenizer = AutoTokenizer.from_pretrained(self._repository_link)
     model = AutoModelForSequenceClassification.from_pretrained(
       self._repository_link,
-      id2label={0: "ham", 1: "spam"}
+      id2label=self._label_identifiers
     )
   
     self.model = pipeline(
@@ -49,7 +50,7 @@ class ClassifierModel:
       return None
     
     tokenizer = AutoTokenizer.from_pretrained(f"{self._local_model_path}")
-    model = AutoModelForSequenceClassification.from_pretrained(f"{self._local_model_path}", id2label={0: "ham", 1: "spam"})
+    model = AutoModelForSequenceClassification.from_pretrained(f"{self._local_model_path}", id2label=self._label_identifiers)
     self.model = pipeline('text-classification', model=model, tokenizer=tokenizer)
   
   def classify(self, text: str):
